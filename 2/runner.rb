de1d12aa661eefa7b5ef84937845ba54b6eb4ea4
Sri Harsha Kappala <sriharsha.k@caratlane.com>
2018-05-29 22:50:12 +0530

    Refactor

--- 2/runner.rb
+++ 2/runner.rb
@@ -15,56 +15,11 @@ STORE_MENU = "
   5. Close Store
 "
 
-BILLING_MENU = "
-  1. Add product to Cart
-  2. View Cart
-  3. Checkout
-  4. Clear Cart
-  5. Exit
-"
-
-store = Store.new('My Awesome Store', 10, 15)
-
 def displayMenu
   puts STORE_MENU
 end
 
-def billingProcess store
-  cart = Cart.new
-  checkout = false
-  while !checkout
-    puts BILLING_MENU
-    input = gets.chomp.to_i
-    case input
-    when 1
-      store.viewInventory
-      puts "Enter product to buy"
-      code = gets.chomp
-      puts "Enter quantity to buy"
-      quantity = gets.chomp.to_i
-      status, error = CartService.new(store).addProductToCart cart, code, quantity
-      if status == -1
-        puts error
-      end
-    when 2
-      cart.view
-    when 3
-      puts DISCOUNT_MENU
-      discountType = gets.chomp.to_i
-      status, error = CartService.new(store).placeOrder cart, discountType
-      if status == -1
-        puts error
-      end
-    when 4
-      CartService.new(store).clearCart cart
-    when 5
-      status, error = CartService.new(store).clearCart cart
-      checkout = true
-    else
-      puts 'Invalid Command'
-    end
-  end
-end
+store = Store.new('My Awesome Store', 10, 15)
 
 while store.open?
   displayMenu
@@ -75,7 +30,6 @@ while store.open?
     when 2
       store.viewInventory
     when 3
-      # billingProcess store
       StoreService.new(store).processBilling
     when 4
       store.viewOrders
